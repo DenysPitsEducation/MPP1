fa4011081f498830be56991e12a909f588256e9d
DenysPitsEducation <99960195+DenysPitsEducation@users.noreply.github.com>
2022-02-18 12:09:35 +0200

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,172 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program1
+    {
+        static void Main1(string[] args)
+        {
+            // number of words to show
+            int n = 25;
+            string text = "White in tigers in live mostly in India Wild lions live mostly in Africa dds";
+            // string filePath = "C:/Users/lykos/Downloads/sample.txt";
+            // string text = File.ReadAllText(filePath);
+
+            int textLength = 0;
+            try
+            {
+                int i = 0;
+            begin1:
+                char c = text[i];
+                textLength = i + 1;
+                i++;
+                goto begin1;
+            }
+            catch (Exception)
+            {
+            }
+
+
+            int wordsArrayLength = 1000000;
+            string[] words = new string[wordsArrayLength];
+
+            // split alternative
+            int emptyWordIndexTemp = 0;
+            int i1 = 0;
+        outer1:
+            if (i1 < textLength)
+            {
+                if (text[i1] >= 65 && text[i1] <= 90 || text[i1] >= 97 && text[i1] <= 122)
+                {
+                    string word = "";
+                    if (text[i1] >= 65 && text[i1] <= 90)
+                    {
+                        word += (char)(text[i1] + 32);
+                    }
+                    else
+                    {
+                        word += text[i1];
+                    }
+                    int k = i1 + 1;
+                inner1:
+                    if (k < textLength)
+                    {
+                        if (text[k] >= 97 && text[k] <= 122)
+                        {
+                            word += text[k];
+                        }
+                        else if (text[k] >= 65 && text[k] <= 90)
+                        {
+                            word += (char)(text[k] + 32);
+                        }
+                        else
+                        {
+                            i1 = k;
+                            goto innerEnd1;
+                        }
+                        k++;
+                        goto inner1;
+                    }
+                innerEnd1:
+                    words[emptyWordIndexTemp] = word;
+                    emptyWordIndexTemp++;
+                }
+                i1++;
+                goto outer1;
+            }
+
+            string[] identicalWords = new string[wordsArrayLength];
+            int[] wordNumbers = new int[wordsArrayLength];
+
+            int i2 = 0;
+        outer2:
+            if (i2 < wordsArrayLength)
+            {
+                if (words[i2] == null)
+                {
+                    goto outerEnd2;
+                }
+                int j2 = 0;
+            inner2:
+                if (j2 < wordsArrayLength)
+                {
+                    if (identicalWords[j2] == words[i2])
+                    {
+                        wordNumbers[j2] = wordNumbers[j2] + 1;
+                        goto outerContinue2;
+                    }
+                    else if (identicalWords[j2] == null)
+                    {
+                        // get length of desired word to add
+                        int identicalWordLength = 0;
+                        try
+                        {
+                            int ii = 0;
+                        begin2:
+                            char c = words[i2][ii];
+                            identicalWordLength = ii + 1;
+                            ii++;
+                            goto begin2;
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        // if it is less than 4, don't add
+                        if (identicalWordLength > 3)
+                        {
+                            identicalWords[j2] = words[i2];
+                            wordNumbers[j2] = 1;
+                        }
+                        goto outerContinue2;
+                    }
+                    j2++;
+                    goto inner2;
+                }
+            outerContinue2:
+                i2++;
+                goto outer2;
+            }
+        outerEnd2:
+            int temp;
+            string tempStr;
+            int identicalWordsLength = 0;
+            int write = 0;
+        outer3:
+            if (write < wordsArrayLength)
+            {
+                if (wordNumbers[write] == 0) goto outerEnd3;
+                int sort = 0;
+            inner3:
+                if (sort < wordsArrayLength - 1)
+                {
+                    if (wordNumbers[sort + 1] == 0)
+                    {
+                        identicalWordsLength = sort + 1;
+                        goto innerEnd3;
+                    }
+                    if (wordNumbers[sort] > wordNumbers[sort + 1])
+                    {
+                        temp = wordNumbers[sort + 1];
+                        wordNumbers[sort + 1] = wordNumbers[sort];
+                        wordNumbers[sort] = temp;
+                        tempStr = identicalWords[sort + 1];
+                        identicalWords[sort + 1] = identicalWords[sort];
+                        identicalWords[sort] = tempStr;
+                    }
+                    sort++;
+                    goto inner3;
+                }
+            innerEnd3:
+                write++;
+                goto outer3;
+            }
+        outerEnd3:
+            int i4 = identicalWordsLength - 1;
+        outer4:
+            if (i4 >= (identicalWordsLength - n >= 0 ? identicalWordsLength - n : 0))
+            {
+                Console.WriteLine(identicalWords[i4] + " - " + wordNumbers[i4]);
+                i4--;
+                goto outer4;
+            }
+        }
+    }
+}
---
+++ task2.cs
@@ -0,0 +1,213 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program2
+    {
+        static void Main2(string[] args)
+        {
+            string[] lines = File.ReadAllLines("C:/Users/lykos/Downloads/sample.txt");
+            int lineHeight = 45;
+
+            string[] pages = new string[1000000];
+            string textTemp = "";
+            int pageIndexTemp = 0;
+            int i1 = 0;
+        outer1:
+            if (i1 < lines.Length)
+            {
+                if (i1 % lineHeight == 0 && i1 != 0)
+                {
+                    pages[pageIndexTemp] = textTemp;
+                    pageIndexTemp++;
+                    textTemp = "";
+                }
+                textTemp += lines[i1] + " ";
+                i1++;
+                goto outer1;
+            }
+
+            string[] words = new string[1000000];
+            int pageLimit = 100;
+            int[][] pageNumbers = new int[1000000][];
+            int i2 = 0;
+        outer2:
+            if (i2 < 1000000)
+            {
+                pageNumbers[i2] = new int[pageLimit + 1];
+                i2++;
+                goto outer2;
+            }
+
+            int i3 = 0;
+        outer3:
+            if (i3 < 1000000)
+            {
+                string pageText = pages[i3];
+                if (pageText == null) goto outerEnd3;
+                string[] pageWords = new string[1000000];
+
+                // split alternative
+                int textLength = pageText.Length;
+                int emptyWordIndexTemp = 0;
+                int j3_1 = 0;
+            inner3_1:
+                if (j3_1 < textLength)
+                {
+                    if (pageText[j3_1] >= 65 && pageText[j3_1] <= 90 || pageText[j3_1] >= 97 && pageText[j3_1] <= 122)
+                    {
+                        string word = "";
+                        word += pageText[j3_1];
+                        int k = j3_1 + 1;
+                    innerInner3_1:
+                        if (k < textLength)
+                        {
+                            if ((pageText[k] >= 65 && pageText[k] <= 90 || pageText[k] >= 97 && pageText[k] <= 122))
+                            {
+                                word += pageText[k];
+                            }
+                            else
+                            {
+                                j3_1 = k;
+                                goto innerInnerEnd3_1;
+                            }
+                            k++;
+                            goto innerInner3_1;
+                        }
+                    innerInnerEnd3_1:
+                        pageWords[emptyWordIndexTemp] = word;
+                        emptyWordIndexTemp++;
+                    }
+                    j3_1++;
+                    goto inner3_1;
+                }
+
+                // main logic
+                int j3_2 = 0;
+            inner3_2:
+                if (j3_2 < pageWords.Length)
+                {
+                    if (pageWords[j3_2] == null) goto innerEnd3_2;
+                    int k3 = 0;
+                innerInner3_2:
+                    if (k3 < words.Length)
+                    {
+                        if (words[k3] == pageWords[j3_2])
+                        {
+                            int l3 = 0;
+                        innerInnerInner3_2:
+                            if (l3 < pageLimit + 1)
+                            {
+                                if (pageNumbers[k3][l3] == i3 + 1) goto innerInnerInnerEnd3_2;
+                                if (pageNumbers[k3][l3] == 0)
+                                {
+                                    pageNumbers[k3][l3] = i3 + 1;
+                                    goto innerInnerInnerEnd3_2;
+                                }
+                                l3++;
+                                goto innerInnerInner3_2;
+                            }
+                        innerInnerInnerEnd3_2:
+                            goto innerInnerEnd3_2;
+                        }
+                        if (words[k3] == null)
+                        {
+                            words[k3] = pageWords[j3_2];
+                            pageNumbers[k3][0] = i3 + 1;
+                            goto innerInnerEnd3_2;
+                        }
+                        k3++;
+                        goto innerInner3_2;
+                    }
+                innerInnerEnd3_2:
+                    j3_2++;
+                    goto inner3_2;
+                }
+            innerEnd3_2:
+                i3++;
+                goto outer3;
+            }
+        outerEnd3:
+
+            // sort
+            string? temp = null;
+            int[]? tempArray = null;
+            int write = 0;
+        outer4:
+            if (write < words.Length)
+            {
+                if (words[write] == null) goto outerEnd4;
+                int sort = 0;
+            inner4:
+                if (sort < words.Length - 1)
+                {
+                    if (words[sort + 1] == null)
+                    {
+                        goto innerEnd4;
+                    }
+                    bool firstLonger = words[sort].Length - words[sort + 1].Length > 0;
+                    bool firstGreater = false;
+                    int i4 = 0;
+                innerInner4:
+                    if (i4 < (firstLonger ? words[sort + 1].Length : words[sort].Length))
+                    {
+                        if (words[sort][i4] > words[sort + 1][i4])
+                        {
+                            firstGreater = true;
+                        }
+                        if (words[sort][i4] != words[sort + 1][i4])
+                        {
+                            goto innerInnerEnd4;
+                        }
+                        i4++;
+                        goto innerInner4;
+                    }
+                innerInnerEnd4:
+                    if (firstGreater)
+                    {
+                        temp = words[sort + 1];
+                        words[sort + 1] = words[sort];
+                        words[sort] = temp;
+                        tempArray = pageNumbers[sort + 1];
+                        pageNumbers[sort + 1] = pageNumbers[sort];
+                        pageNumbers[sort] = tempArray;
+                    }
+                    sort++;
+                    goto inner4;
+                }
+            innerEnd4:
+                write++;
+                goto outer4;
+            }
+        outerEnd4:
+
+            // print
+            int i5 = 0;
+        outer5:
+            if (i5 < words.Length)
+            {
+                if (words[i5] == null) goto outerEnd5;
+                if (pageNumbers[i5][pageLimit] != 0) goto outerContinue5;
+                string output = words[i5] + " - ";
+                int j5 = 0;
+            inner5:
+                if (j5 < pageLimit)
+                {
+                    if (pageNumbers[i5][j5] == 0) goto innerEnd5;
+                    output += pageNumbers[i5][j5];
+                    if (j5 != pageLimit - 1 && pageNumbers[i5][j5 + 1] != 0)
+                    {
+                        output += ", ";
+                    }
+                    j5++;
+                    goto inner5;
+                }
+            innerEnd5:
+                Console.WriteLine(output);
+            outerContinue5:
+                i5++;
+                goto outer5;
+            }
+        outerEnd5:
+            int end;
+        }
+    }
+}
